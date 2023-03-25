2527a07b7245ea61f3493b8a7302d3dc2bc5c959
Izu-TABI <masakisama14@gmail.com>
2023-03-25 17:26:02 +0900

    create dotfiles

---
+++ .config/nvim/after/plugin/mason-lspconfig.rc.lua
@@ -0,0 +1,4 @@
+local nvim_lsp = require "lspconfig"
+nvim_lsp.tailwindcss.setup {}
+nvim_lsp.clangd.setup {}
+nvim_lsp.asm_lsp.setup {}
---
+++ .config/nvim/after/plugin/mason.rc.lua
@@ -0,0 +1,11 @@
+if (not status) then return end
+local status2, lspconfig = pcall(require, "mason-lspconfig")
+if (not status2) then return end
+
+mason.setup({
+
+})
+
+lspconfig.setup {
+  ensure_installed = { "sumneko_lua", "tailwindcss" },
+}
